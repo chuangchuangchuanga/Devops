473c1333bdce4e7aaac897095734e1bee3574863
wangchuang <mail.wangchuang@gmail.com>
2017-07-05 11:17:20 +0800

    no message

--- apps/git/script.py
+++ apps/git/script.py
@@ -15,11 +15,11 @@ def fun(ipadd, path, reset):
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     s.connect(ipadd, 22, user, passwd, timeout=5)
     if reset == 'update':
-        stdin, stdout, stderr = s.exec_command('cd %s ;sudo git fetch origin master; sudo git merge origin/master' %path, get_pty=True)
+        stdin, stdout, stderr = s.exec_command('cd %s ;sudo git fetch origin master:temp;sudo git merge temp' %path, get_pty=True)
     else:
         stdin, stdout, stderr = s.exec_command('cd %s ;sudo git reset --hard %s' %(path,reset), get_pty=True)
     cmd_result = stdout.read(), stderr.read()
-    #for line in cmd_result:
-    #     print line
+    for line in cmd_result:
+         print line
     s.close()
-    print cmd_result
+#    print cmd_result
